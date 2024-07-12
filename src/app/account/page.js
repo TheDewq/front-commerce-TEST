1598b808c9fad68c51a269485ccf76174e59e8d2
Carlos O <orjuelacarlos80@gmail.com>
2024-07-12 14:16:32 -0500

    tabs listos

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,9 +1,18 @@
 "use client";
 import  BasicInfo  from "@/components/account/basic-info/basic-info";
-import Tabs from "@/components/account/tabs/Tabs";
+import Tabs from "@/components/account/tabs/tabs";
+import { useAuth } from "@/hooks";
 import MainLayout from "@/layout/main/main-layout";
+import { useRouter } from "next/navigation";
 
 export default function Account(){
+    const router = useRouter();
+    const {user} = useAuth();
+
+    if(!user){
+        router.push("/");
+        return;
+    }
     return(
         <>
             <MainLayout>
